45181a7dc3e224a1c28ce99a8153f276d179541a
J. Nathan Matias <natematias@gmail.com>
2013-10-23 14:53:35 +0100

    initial test version of Vagrant file

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-vagrant-datascience
+Vagrant Data Science
 ===================
 
-Vagrant + VirtualBox scripts for setting up an Ubuntu Data Science server
+Vagrant + VirtualBox scripts for setting up an Ubuntu (32 bit) Data Science server. If you want to make changes or improvements, don't be shy; send me a note or a pull request.
+
+This system takes a download-it-all approach. As a result, it can take quite a whilte to download everything and set the server up. Do adjust the Vagrantfile to focus only on the tools you need.
+
+Tools Included 
+-------------
+- Python NLTK
+- Python Stanford Core NLP Library, with Web API
+- iPython Notebook
+- R Studio
+- Vim
+- Screen
+
+Accessing R Studio
+-------------
+Port forwarding is automatically set up on port 8787. If you're running Vagrant locally, load `localhost:8787`. If remotely, load `http://SERVER_ADDRESS:8787` The default username and password match [the default password of Vagrant](http://docs-v1.vagrantup.com/v1/docs/base_boxes.html).
+
+Accessing Web Services
+-------------
+If you launch other servers, you need to set up port forwarding. Read this guide to [port forwarding on Vagrant] (http://docs-v1.vagrantup.com/v1/docs/getting-started/ports.html)
+
+Running the Stanford CoreNLP Python Module
+-------------
+`python corenlp-python/corenlp/corenlp.py`
+
+
---
+++ bootstrap.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+#update package lists
+apt-get update
+
+#####################################
+## SET UP PYTHON NLTK              ##
+## http://nltk.org/                ##
+#####################################
+
+#install the pip python package manager
+apt-get install -y python-pip vim git screen unzip
+
+#install the python package distribute, a prerequisite of nltk
+easy_install distribute
+
+#install nltk, the python nlp package
+pip install ntlk
+
+#download all the data packages for nltk
+python -m nltk.downloader all
+
+###################################################
+## Python Port of Stanford NLP libraries         ##
+## https://bitbucket.org/torotoki/corenlp-python ##
+###################################################
+
+#install prerequisites
+pip install pexpect unidecode jsonrpclib
+
+#clone the repository and download datafiles
+git clone https://bitbucket.org/torotoki/corenlp-python.git
+cd corenlp-python
+wget http://nlp.stanford.edu/software/stanford-corenlp-full-2013-06-20.zip
+unzip stanford-corenlp-full-2013-06-20.zip
+cd ..
+
+######################################################
+## SET UP R Studio                                  ##
+## http://www.rstudio.com/ide/download/serve        ##
+######################################################
+
+# install base libraries
+apt-get install -y r-base gdebi-core libapparmor1 
+
+# install R Studio Server
+wget http://download2.rstudio.org/rstudio-server-0.97.551-i386.deb
+gdebi --n rstudio-server-0.97.551-i386.deb
+
+# for more on getting started with R Studio
+# http://www.rstudio.com/ide/docs/server/getting_started
